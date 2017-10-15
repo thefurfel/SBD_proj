cae287e5671e45deff35ad203227a5ae69d2f64b
Furfel <furfelbugs@hotmail.com>
2017-10-15 18:21:04 +0200

    Pierwszy push

---
+++ src/pg/ppabis/bazy1/Main.java
@@ -0,0 +1,15 @@
+package pg.ppabis.bazy1;
+
+
+/*
+ * 2017 (c) Piotr Pabis 149528 Grupa 6B
+ * */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/pg/ppabis/bazy1/Rekord.java
@@ -0,0 +1,34 @@
+package pg.ppabis.bazy1;
+
+import java.util.Random;
+
+/*
+ * 2017 (c) Piotr Pabis 149528 Grupa 6B
+ * */
+
+public class Rekord {
+	float a,b,h;
+	
+	public Rekord(float a, float b, float wysokosc) {
+		this.a=a;
+		this.b=b;
+		this.h=wysokosc;
+	}
+	
+	public float objetosc() {
+		return a*b*h;
+	}
+	
+	
+	public static final Random RandomGenerator = new Random();
+	public static final float MIN_PODSTAWA = 2f;
+	public static final float MAX_PODSTAWA = 10f;
+	public static final float MIN_WYSOKOSC = 10f;
+	public static final float MAX_WYSOKOSC = 30f;
+	public static Rekord losowy() {
+		return new Rekord(
+				MIN_PODSTAWA+RandomGenerator.nextFloat()*MAX_PODSTAWA,
+				MIN_PODSTAWA+RandomGenerator.nextFloat()*MAX_PODSTAWA,
+				MIN_WYSOKOSC+RandomGenerator.nextFloat()*MAX_WYSOKOSC);
+	}
+}
