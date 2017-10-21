a3eb3c37e9d7c453a60b5effde151658dec18abe
Furfel <furfelbugs@hotmail.com>
2017-10-21 16:05:35 +0200

    Some cleanup

--- src/pg/ppabis/bazy1/Main.java
+++ src/pg/ppabis/bazy1/Main.java
@@ -17,31 +17,40 @@ public class Main {
 	public static void main(String[] args) {
 		if(args.length<1) {
 			System.out.println("Usage: Main <COMMAND> [OPTIONS]");
+			System.out.println("\nCommands:\n\tcreate <FILE> <NUM> - tworzy NUM losowych rekordow do pliku  <FILE>\n\tview <FILE> - wyswietla zawartosc <FILE>");
 		} else {
 			if(args[0].equalsIgnoreCase("create")) {
-				if(args.length>2){
-					Tasma tasma = new Tasma(new File(args[1]));
-					tasma.openAsOutput();
-					int number = Integer.parseInt(args[2]);
-					for(int i=0;i<number;++i) {
-						tasma.writeNext(Rekord.losowy());
-					}
-					tasma.flush();
-				} else {
-					System.err.println("use create file.bin number");
-				}
+				create(args);
 			} else if(args[0].equalsIgnoreCase("view")) {
-				if(args.length>1) {
-					Tasma tasma = new Tasma(new File(args[1]));
-					tasma.openAsInput();
-					Rekord r = tasma.readNext();
-					while(r!=null) {
-						System.out.println(r);
-						r = tasma.readNext();
-					}
-				} else {System.err.println("use view file.bin");}
+				view(args);
 			}
 		}
+	}
+	
+	public static void create(String[] args) {
+		if(args.length>2){
+			Tasma tasma = new Tasma(new File(args[1]));
+			tasma.openAsOutput();
+			int number = Integer.parseInt(args[2]);
+			for(int i=0;i<number;++i) {
+				tasma.writeNext(Rekord.losowy());
+			}
+			tasma.flush();
+		} else {
+			System.err.println("use create file.bin number");
+		}
+	}
+	
+	public static void view(String[] args) {
+		if(args.length>1) {
+			Tasma tasma = new Tasma(new File(args[1]));
+			tasma.openAsInput();
+			Rekord r = tasma.readNext();
+			while(r!=null) {
+				System.out.println(r);
+				r = tasma.readNext();
+			}
+		} else {System.err.println("use view file.bin");}
 	}
 
 }
